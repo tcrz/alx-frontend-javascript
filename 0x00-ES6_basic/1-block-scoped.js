217c355ee84ef09aed514ff4b0b994f16787e7b1
Tonycrz <tonybaidoo0@gmail.com>
2022-06-14 10:09:40 +0000

    1-block-scoped.js

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,11 @@
+export default function taskBlock(trueOrFalse) {
+  var task11 = false;
+  var task22 = true;
+
+  if (trueOrFalse) {
+    var task = true;
+    var task2 = false;
+  }
+
+  return [task11, task22];
+}
