191964c9e1d1c2b000951c6690b99ccd8d72c041
Tonycrz <tonybaidoo0@gmail.com>
2022-06-15 12:42:40 +0000

    disable line errors for unused vars

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task22 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    const task = true; // eslint-disable-line no-unused-vars
+    const task2 = false; // eslint-disable-line no-unused-vars
   }
 
   return [task11, task22];
