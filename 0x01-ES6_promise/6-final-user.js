a08df7d7669d0bab8acd90f1aa913a584cb52fea
Tonycrz <tonybaidoo0@gmail.com>
2022-06-15 18:49:27 +0000

    6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -4,5 +4,9 @@ import uploadPhoto from './5-photo-reject';
 export default function handleProfileSignup(firstName, lastName, fileName) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(fileName);
-  return Promise.allSettled([user, photo]).then((results) => results);
+  let r = []
+ return Promise.allSettled([user, photo]).then((results) => {
+    return results
+  });
+  console.log(r)
 }
