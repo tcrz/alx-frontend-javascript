6a050e85060e011683fb5eeb7ea1b1aed5270179
Tonycrz <tonybaidoo0@gmail.com>
2022-06-15 13:44:47 +0000

    3-all.js

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,12 @@
+import { uploadPhoto, createUser } from "./utils";
+
+
+export default function handleProfileSignup() {
+  const photo = uploadPhoto()
+  const user = createUser()
+
+  Promise.all([photo, user]).then((value) => {
+    console.log(value[0].body, value[1].firstName, value[1].lastName)
+ }).catch(() => console.log('Signup system offline'))
+
+}
