36c418538fcbc232834c66cd16b9af2db17e55b3
Tonycrz <tonybaidoo0@gmail.com>
2022-06-21 10:03:37 +0000

    updates

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -22,9 +22,9 @@ export function queryAPI(endpoint) {
   if (!weakMap.get(endpoint)) {
     weakMap.set(endpoint, 1);
   } else {
-      weakMap.set(endpoint, weakMap.get(endpoint) + 1);
-      if (weakMap.get(endpoint) >= 5) {
-        throw new Error('Endpoint load is high');
-      }
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+    if (weakMap.get(endpoint) >= 5) {
+      throw new Error('Endpoint load is high');
+    }
   }
 }
