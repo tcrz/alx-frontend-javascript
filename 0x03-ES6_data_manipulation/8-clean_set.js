d50db71437f917ce5cef0f8aa2f717020eef0fdf
Tonycrz <tonybaidoo0@gmail.com>
2022-06-21 19:46:22 +0000

    clear_commented_code

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -20,18 +20,3 @@ export default function cleanSet(set, startString) {
   }
   return concatStr;
 }
-
-// export default function cleanSet(set, startString) {
-//   let text = '';
-//   const array = [];
-
-//   if (startString && typeof startString === 'string') {
-//     for (const element of set) {
-//       if (element && element.startsWith(startString)) {
-//         array.push(element.slice(startString.length));
-//       }
-//     }
-//     text = array.join('-');
-//   }
-//   return text;
-// }
